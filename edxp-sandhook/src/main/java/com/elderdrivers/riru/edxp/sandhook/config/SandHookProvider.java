2d02297143d3041003668c2e54831cef9d9b1951
solohsu <hsushulong@gmail.com>
2019-03-26 17:44:50 +0800

    Pull out common codes

--- edxp-sandhook/src/main/java/com/elderdrivers/riru/edxp/sandhook/config/SandHookProvider.java
+++ edxp-sandhook/src/main/java/com/elderdrivers/riru/edxp/sandhook/config/SandHookProvider.java
@@ -2,10 +2,10 @@ package com.elderdrivers.riru.edxp.sandhook.config;
 
 import android.util.Log;
 
-import com.elderdrivers.riru.edxp.hook.HookProvider;
+import com.elderdrivers.riru.edxp.config.BaseHookProvider;
+import com.elderdrivers.riru.edxp.deopt.PrebuiltMethodsDeopter;
 import com.elderdrivers.riru.edxp.sandhook.dexmaker.DexMakerUtils;
 import com.elderdrivers.riru.edxp.sandhook.dexmaker.DynamicBridge;
-import com.elderdrivers.riru.edxp.sandhook.util.PrebuiltMethodsDeopter;
 import com.swift.sandhook.xposedcompat.XposedCompat;
 import com.swift.sandhook.xposedcompat.methodgen.SandHookXposedBridge;
 
@@ -14,7 +14,7 @@ import java.lang.reflect.Member;
 
 import de.robv.android.xposed.XposedBridge;
 
-public class SandHookProvider implements HookProvider {
+public class SandHookProvider extends BaseHookProvider {
     @Override
     public void hookMethod(Member method, XposedBridge.AdditionalHookInfo additionalInfo) {
         if (SandHookXposedBridge.hooked(method) || DynamicBridge.hooked(method)) {
@@ -43,15 +43,5 @@ public class SandHookProvider implements HookProvider {
     @Override
     public Member findMethodNative(Member hookMethod) {
         return DexMakerUtils.findMethodNative(hookMethod);
-    }
-
-    @Override
-    public void deoptMethods(String packageName, ClassLoader classLoader) {
-        PrebuiltMethodsDeopter.deoptMethods(packageName, classLoader);
-    }
-
-    @Override
-    public long getMethodId(Member member) {
-        return 0;
     }
 }
